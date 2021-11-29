edd8ff90f2b9aa156406d80790a0e96f110f09f6
Hibi <69405847+Hibiii@users.noreply.github.com>
2021-11-29 19:07:05 -0300

    Add redstone sparking

--- src/main/java/hibi/aestheticsfix/Init.java
+++ src/main/java/hibi/aestheticsfix/Init.java
@@ -1,8 +1,12 @@
 package hibi.aestheticsfix;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
 
 public class Init implements ClientModInitializer {
+
+	public static final SoundEvent REDSTONE_SPARK = new SoundEvent(new Identifier("aestheticsfix", "redstone_spark"));
 
 	@Override
 	public void onInitializeClient() {
--- src/main/java/hibi/aestheticsfix/mix/RedstoneWireBlockMixin.java
+++ src/main/java/hibi/aestheticsfix/mix/RedstoneWireBlockMixin.java
@@ -7,21 +7,22 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import hibi.aestheticsfix.EffectDelegate;
-import hibi.aestheticsfix.Effects;
-import net.minecraft.block.Block;
+import hibi.aestheticsfix.Init;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.RedstoneWireBlock;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-@Mixin(Block.class)
-public class BlockMixin {
+@Mixin(RedstoneWireBlock.class)
+public class RedstoneWireBlockMixin {
 	@Inject(
-		at = @At("HEAD"),
+		at = @At("TAIL"),
 		method = "randomDisplayTick(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Ljava/util/Random;)V"
 	)
 	private void displayTick(BlockState state, World world, BlockPos pos, Random random, CallbackInfo info) {
-		EffectDelegate delegate = Effects.get(state.getBlock());
-		if(delegate != null) delegate.displayTick(state, world, pos, random);
+		if(state.get(Properties.POWER) > 1 && random.nextInt(1500) == 0)
+		world.playSound(pos.getX(), pos.getY(), pos.getZ(), Init.REDSTONE_SPARK, SoundCategory.BLOCKS, state.get(Properties.POWER) / 15f, 1.0f, true);
 	}
 }
