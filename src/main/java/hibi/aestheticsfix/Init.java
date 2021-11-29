38fa55cf612c3ec45a78380c4588d392afefc436
Hibi <69405847+Hibiii@users.noreply.github.com>
2021-11-29 15:05:53 -0300

    Basic branding

---
+++ src/main/java/hibi/aestheticsfix/Init.java
@@ -0,0 +1,10 @@
+package hibi.aestheticsfix;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Init implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+	}
+}
--- src/main/java/hibi/aestheticsfix/mix/ExampleMixin.java
+++ src/main/java/hibi/aestheticsfix/mix/ExampleMixin.java
@@ -1,16 +1,18 @@
-package net.fabricmc.example.mixin;
+package hibi.aestheticsfix.mix;
 
-import net.fabricmc.example.ExampleMod;
-import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.client.gui.screen.TitleScreen;
+
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
+	@Inject(
+		at = @At("HEAD"),
+		method = "init"
+	)
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
